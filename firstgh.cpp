346ae85b6036dde31cb2ef8ffa7dd8fd8e4fb551
shakilcr7 <gmshakil17@gmail.com>
2023-10-20 21:51:16 +0600

    pair of number check function included

--- firstgh.cpp
+++ firstgh.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
+//checking the function that has the pair of the number passed in second parameter
+bool check_pair(int arr[],int num){
+    for(int i=0;i<num;i++){
+        for(int j=0;j<num;j++){
+            if(i != j && arr[i]+arr[j]==num){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 int main() {
-    std::cout << "Hello World!";
+    int arr[5] = {5,8,6,1,4};
+
+    bool result = check_pair(arr,5);
+    cout<<result<<endl;
     return 0;
 }
